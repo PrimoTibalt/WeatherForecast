ca661c7c7a748abc1a3238b280557ffb05382d0a
= <anton.shheglov.1@gmail.com>
2020-08-25 19:21:11 +0300

    Deleted unnecessary field of FetchWeather.

--- lib/Bloc/Events/FetchWeather.dart
+++ lib/Bloc/Events/FetchWeather.dart
@@ -3,10 +3,6 @@ import 'package:weather/Models/Location/Location.dart';
 import 'WeatherEvent.dart';
 
 class FetchWeather extends WeatherEvent {
-  final Location _location;
-
-  FetchWeather(this._location);
-
   @override
-  List<Object> get props => [this._location];
+  List<Object> get props => [];
 }
