db14bb44ee4fec28babf56643d35776656019e4e
= <anton.shheglov.1@gmail.com>
2020-08-25 19:19:33 +0300

    Fix bug of infinite loading.

--- lib/Bloc/WeatherBloc.dart
+++ lib/Bloc/WeatherBloc.dart
@@ -5,9 +5,9 @@ import 'package:weather/Repositories/WeatherRepository.dart';
 
 import 'Events/FetchWeather.dart';
 import 'Events/WeatherEvent.dart';
-import 'States/WeatherIsLoaded.dart';
 import 'States/WeatherIsLoading.dart';
 import 'States/WeatherIsNotLoaded.dart';
+import 'States/WeatherList.dart';
 import 'States/WeatherState.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
@@ -19,13 +19,11 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   @override
   Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
     if (event is FetchWeather) {
-      yield WeatherIsLoading();
-
       try {
         List<WeatherModel> weather = await this
             .weatherRepo
             .getWeather(await LocationRepo().getCurrentLocation());
-        yield WeatherIsLoaded(weather);
+        yield WeatherList(weather);
       } catch (e) {
         yield WeatherIsNotLoaded();
       }
