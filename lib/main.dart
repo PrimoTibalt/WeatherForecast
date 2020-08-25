57b4aae6d82822435108613eb658029dee439e37
= <anton.shheglov.1@gmail.com>
2020-08-25 19:22:52 +0300

    Add view of tempreture in list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/Bloc/Events/FetchWeather.dart';
+import 'package:weather/Bloc/States/WeatherCurrent.dart';
+import 'package:weather/Bloc/States/WeatherIsLoading.dart';
+import 'package:weather/Bloc/States/WeatherIsNotLoaded.dart';
+import 'package:weather/Models/Weather/WeatherModel.dart';
+import 'package:weather/Pages/WeatherIsLoadingView.dart';
+import 'package:weather/Repositories/LocationRepository.dart';
+import 'package:weather/Repositories/WeatherRepository.dart';
+
+import 'Bloc/States/WeatherList.dart';
+import 'Bloc/States/WeatherState.dart';
+import 'Bloc/WeatherBloc.dart';
+import 'Pages/WeatherListView.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,16 +26,33 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           resizeToAvoidBottomInset: false,
-          backgroundColor: Colors.grey[900],
-          body: ListOfWeather(),
+          backgroundColor: Colors.white,
+          body: BlocProvider(
+            create: (context) => WeatherBloc(WeatherIsLoading(), WeatherRepo()),
+            child: Home(),
+          ),
         ));
   }
 }
 
-class ListOfWeather extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final weatherBloc = BlocProvider.of<WeatherBloc>(context);
+    return Container(
+      child: BlocBuilder<WeatherBloc, WeatherState>(builder: (context, state) {
+        if (state is WeatherIsLoading) {
+          return WeatherIsLoadingView(weatherBloc);
+        } else if (state is WeatherList) {
+          return WeatherListView(state.getWeather);
+        } else if (state is WeatherCurrent) {
+          throw UnimplementedError();
+        } else if (state is WeatherIsNotLoaded) {
+          throw UnimplementedError();
+        } else {
+          throw UnimplementedError('Unexpected state!');
+        }
+      }),
+    );
   }
 }
