94b5f92de03ee45b76a3ade4dab3b8c14f974beb
= <anton.shheglov.1@gmail.com>
2020-08-26 15:25:47 +0300

    Add WeatherListView.

--- lib/Pages/WeatherListView.dart
+++ lib/Pages/WeatherListView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather/Models/Weather/WeatherModel.dart';
+import 'package:weather/Services/WeatherRowService.dart';
 
 class WeatherListView extends StatelessWidget {
   final List<WeatherModel> listOfWeather;
@@ -11,9 +12,14 @@ class WeatherListView extends StatelessWidget {
     return ListView.builder(
       itemCount: this.listOfWeather.length,
       itemBuilder: (context, index) {
-        return Text(
-          this.listOfWeather[index].data.temp.toString(),
-        );
+        return Semantics(
+            button: false,
+            focusable: true,
+            child: Padding(
+              child: WeatherRowService(this.listOfWeather[index], context)
+                  .getRow(),
+              padding: EdgeInsets.all(20),
+            ));
       },
     );
   }
