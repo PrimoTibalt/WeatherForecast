36db8c945d5b55cbd23559312ce1fa811c0290b8
= <anton.shheglov.1@gmail.com>
2020-08-26 15:22:54 +0300

    Add WeatherRowService.

---
+++ lib/Services/WeatherRowService.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:weather/Models/Weather/WeatherModel.dart';
+
+import 'WeatherIconService.dart';
+
+class WeatherRowService {
+  final WeatherModel weather;
+  final BuildContext context;
+
+  WeatherRowService(
+    this.weather,
+    this.context,
+  );
+
+  Row getRow() {
+    return Row(
+      children: [
+        Container(
+          child: (WeatherIconService(weather.general.description)).getIcon(),
+          width: MediaQuery.of(context).size.width * 0.15,
+          padding: EdgeInsets.only(right: 10),
+        ),
+        Container(
+          child: Row(
+            children: [
+              Container(
+                child: Column(
+                  children: [
+                    Text(weather.additional.time),
+                    Text(weather.general.weather),
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+                width: MediaQuery.of(context).size.width * 0.45,
+              ),
+              Container(
+                child: Text(
+                  weather.data.celsiusTemp,
+                  style: TextStyle(
+                      fontSize: 42,
+                      color: Colors.blue[700],
+                      fontFamily: 'Brandon'),
+                  textAlign: TextAlign.end,
+                ),
+              )
+            ],
+          ),
+          decoration: BoxDecoration(
+              border: Border(
+            bottom: BorderSide(color: Colors.grey[300]),
+          )),
+        ),
+      ],
+      crossAxisAlignment: CrossAxisAlignment.center,
+    );
+  }
+}
