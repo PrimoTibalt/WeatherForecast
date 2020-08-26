6113d6ff5fe480f05de094583aee57491eb48282
= <anton.shheglov.1@gmail.com>
2020-08-26 18:41:06 +0300

    Add windDirection prop.

--- lib/Models/Weather/WeatherAdditional.dart
+++ lib/Models/Weather/WeatherAdditional.dart
@@ -15,6 +15,26 @@ class WeatherAdditional {
     return dtTxt.toString().substring(11, 16);
   }
 
+  String get windDirection {
+    return (windDeg > 337.5 || windDeg < 22.5)
+        ? 'N'
+        : (windDeg >= 22.5 && windDeg <= 67.5)
+            ? 'NE'
+            : (windDeg > 67.5 && windDeg < 111.5)
+                ? 'E'
+                : (windDeg >= 111.5 && windDeg <= 156.5)
+                    ? 'SE'
+                    : (windDeg > 156.5 && windDeg < 201.5)
+                        ? 'S'
+                        : (windDeg >= 201.5 && windDeg <= 246.5)
+                            ? 'SW'
+                            : (windDeg > 246.5 && windDeg < 291.5)
+                                ? 'W'
+                                : (windDeg >= 291.5 && windDeg <= 337.5)
+                                    ? 'NW'
+                                    : windDeg.toString() + '°';
+  }
+
   factory WeatherAdditional.fromJson(Map<String, dynamic> json) {
     return WeatherAdditional(
       json['clouds']['all'],
