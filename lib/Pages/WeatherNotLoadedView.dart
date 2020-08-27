c31235d179233610e4621b50f22c882919de3d7b
= <anton.shheglov.1@gmail.com>
2020-08-27 14:43:37 +0300

    Add WeatherNotLoadidView.

---
+++ lib/Pages/WeatherNotLoadedView.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:weather/Services/WeatherErrorMessageService.dart';
+
+class WeatherNotLoadedView extends StatelessWidget {
+  final error;
+
+  WeatherNotLoadedView(this.error);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/icons/Weather/PNG/512/hotSunSunnyWeather.png',
+            scale: 3.0,
+            height: 160,
+            width: 160,
+            color: Colors.grey[400],
+          ),
+          Container(
+            child: Text(WeatherErrorMessageService.beutifulVersion(error),
+                style: TextStyle(fontSize: 30, color: Colors.black87)),
+          )
+        ],
+      ),
+    );
+  }
+}
