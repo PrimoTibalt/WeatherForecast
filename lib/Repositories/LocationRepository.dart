c8526892f37cbcad6a4044351392f6edb193edca
= <anton.shheglov.1@gmail.com>
2020-08-28 15:39:14 +0300

    Add exception if geolocator is off.

--- lib/Repositories/LocationRepository.dart
+++ lib/Repositories/LocationRepository.dart
@@ -3,6 +3,9 @@ import 'package:weather/Models/Location/Location.dart';
 
 class LocationRepo {
   Future<Location> getCurrentLocation() async {
+    if (!(await Geolocator().isLocationServiceEnabled())) {
+      throw Exception('Geolocator isn\'t available.\nPlease, turn it on.');
+    }
     final Position loc = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.lowest);
     return Location(loc.latitude.round(), loc.longitude.round());
