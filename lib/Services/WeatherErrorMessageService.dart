a07a9f8e0fe4250b1db8ec4660f4001156763889
= <anton.shheglov.1@gmail.com>
2020-08-28 15:40:08 +0300

    Add beutiful message on turned off geolocation.

--- lib/Services/WeatherErrorMessageService.dart
+++ lib/Services/WeatherErrorMessageService.dart
@@ -2,6 +2,10 @@ class WeatherErrorMessageService {
   static String beutifulVersion(String errorMessage) {
     if (errorMessage.toUpperCase().contains('FAILED HOST LOOKUP')) {
       return 'You\'re offline';
+    } else if (errorMessage
+        .toUpperCase()
+        .contains('GEOLOCATOR ISN\'T AVAILABLE.')) {
+      return 'Turn on Geolocator';
     } else {
       return errorMessage;
     }
